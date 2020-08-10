b67f992294524c5d64a5ac311ee3efbf76961236
Amr Mohamed <am1066903@gmail.com>
2020-08-10 19:08:58 +0200

    Updates

---
+++ precache-manifest.c647d3b45a2809057f51995d050db342.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73292f6c76d8ceea49c2fd80b4d0b61",
+    "url": "//amrmuhamedd.github.io/market/index.html"
+  },
+  {
+    "revision": "0af6964633eef5479bce",
+    "url": "//amrmuhamedd.github.io/market/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c6d76f022e15b422ddf3",
+    "url": "//amrmuhamedd.github.io/market/static/css/main.6bbe4370.chunk.css"
+  },
+  {
+    "revision": "0af6964633eef5479bce",
+    "url": "//amrmuhamedd.github.io/market/static/js/2.9e5c5698.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "//amrmuhamedd.github.io/market/static/js/2.9e5c5698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d76f022e15b422ddf3",
+    "url": "//amrmuhamedd.github.io/market/static/js/main.401abc3c.chunk.js"
+  },
+  {
+    "revision": "8f845fbd9683b49cc3d8",
+    "url": "//amrmuhamedd.github.io/market/static/js/runtime-main.f770e5b7.js"
+  },
+  {
+    "revision": "1e356b72d8f89048ea96369eed36b65d",
+    "url": "//amrmuhamedd.github.io/market/static/media/bag-1.1e356b72.jpg"
+  },
+  {
+    "revision": "b254084c6ebd17b077fa7b2f2c69fc16",
+    "url": "//amrmuhamedd.github.io/market/static/media/headphone-1.b254084c.jpg"
+  },
+  {
+    "revision": "6723c5ab130c9a1e7fffccbc68a763d4",
+    "url": "//amrmuhamedd.github.io/market/static/media/laptop-1.6723c5ab.jpg"
+  },
+  {
+    "revision": "3cc42044a26ec66410c49f12a6eac73c",
+    "url": "//amrmuhamedd.github.io/market/static/media/shoes2.3cc42044.jpg"
+  },
+  {
+    "revision": "b15cd7196ea72ee93659f572a8eeb7af",
+    "url": "//amrmuhamedd.github.io/market/static/media/tishrt-2.b15cd719.jpg"
+  },
+  {
+    "revision": "821d4dd5a1ee91489e39fd7f7cb4b5ef",
+    "url": "//amrmuhamedd.github.io/market/static/media/watch-1.821d4dd5.jpg"
+  }
+]);
